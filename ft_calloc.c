69588c40ed4c56095e30c70a912dd8c3398e3857
Orhan Tanovic <otanovic@f0r2s9.codam.nl>
2024-11-22 11:34:02 +0100

    hoho

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: otanovic <otanovic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:56:45 by otanovic          #+#    #+#             */
-/*   Updated: 2024/11/22 11:23:39 by otanovic         ###   ########.fr       */
+/*   Updated: 2024/11/22 11:33:57 by otanovic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	*ft_calloc(size_t num_elements, size_t size_of_element)
 	size_t			i;
 
 	i = 0;
-	c = (unsigned char *)malloc(num_elements * size_of_element);
 	if (num_elements == 0 || size_of_element == 0)
 		return (NULL);
+	c = (unsigned char *)malloc(num_elements * size_of_element);
 	if (c == NULL)
 		return (NULL);
 	while (i < num_elements * size_of_element)
