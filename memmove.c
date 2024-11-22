2275d0910ece7824207e3b7d2809103562e6b4bd
Orhan Tanovic <otanovic@f0r2s9.codam.nl>
2024-11-22 11:32:56 +0100

    hoho

--- memmove.c
+++ memmove.c
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memmoveREWRITE.c                                   :+:      :+:    :+:   */
+/*   memmove.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: otanovic <otanovic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 17:49:36 by otanovic          #+#    #+#             */
-/*   Updated: 2024/11/21 15:14:17 by otanovic         ###   ########.fr       */
+/*   Updated: 2024/11/22 11:32:25 by otanovic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
 
-void
-	*ft_memmove(
-			void *dest,
-			const void *src,
-			size_t num
-			)
+void *ft_memmove(void *dest,const void *src,size_t num)
 {
-	unsigned char *d;
-	unsigned char *s;
-	int i;
+	unsigned char	*d;
+	unsigned char	*s;
+	int				i;
 
 	if (dest == src || num == 0)
 	{
