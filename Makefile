a3275be124c1b4af09fc6b0e4333705997db535f
Orhan Tanovic <otanovic@f1r2s9.codam.nl>
2024-11-26 13:37:09 +0100

    striteri done

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: otanovic <otanovic@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/21 17:16:14 by otanovic          #+#    #+#              #
-#    Updated: 2024/11/26 12:37:29 by otanovic         ###   ########.fr        #
+#    Updated: 2024/11/26 13:26:22 by otanovic         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,7 +20,7 @@ SRC = ft_atoi.c ft_calloc.c ft_memcmp.c ft_strrchr.c ft_strchr.c bzero.c isalnum
 		isalpha.c isascii.c isdigit.c isprint.c memcpy.c memmove.c memset.c \
 		strlcat.c strlcpy.c strlen.c bzero.c ft_strdup.c ft_strncmp.c ft_strnstr.c ft_tolower.c \
 		ft_toupper.c ft_strjoin.c ft_split.c ft_strtrim.c memcpy.c ft_substr.c ft_itoa.c ft_memchr.c \
-		ft_strmapi.c
+		ft_strmapi.c ft_striteri.c
 
 OBJ = $(SRC:.c=.o)
 
--- ft_striteri.c
+++ ft_striteri.c
@@ -1,32 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: otanovic <otanovic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/26 12:30:28 by otanovic          #+#    #+#             */
-/*   Updated: 2024/11/26 13:20:21 by otanovic         ###   ########.fr       */
+/*   Created: 2024/11/26 13:20:43 by otanovic          #+#    #+#             */
+/*   Updated: 2024/11/26 13:36:56 by otanovic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <libft.h>
-
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	char	*s2;
 	int		i;
 
 	i = 0;
-	s2 = (char *) malloc(ft_strlen(s) + 1);
-	if (!s2)
-		return (NULL);
+	if (!s && !f)
+		return ;
 	while (s[i] != '\0')
 	{
-		s2[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	s2[i] = '\0';
-	return (s2);
 }
